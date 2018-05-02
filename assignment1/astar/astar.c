ae4af28abc1f44894764ad5fc1eeadd8def20d99
E.E. Boere <s3005690@fwn-bborg-edu-74-33.fmnsedu.rug.nl>
2018-05-02 16:01:41 +0200

    actually saved astar.c, committing again

--- assignment1/astar/astar.c
+++ assignment1/astar/astar.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
 
 #define N 500   /* N times N chessboard */
 
@@ -7,6 +9,7 @@ int actions[8][2] = {  /* knight moves */
   {-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, {1,-2}, {1,2}, {2, -1}, {2, 1}
 };
 int costShortestPath[N][N];
+bool visitedLocation[N][N]; //false means unvisited, true means visited
 
 int isValidLocation(int x, int y) {
   return (0<=x && x < N && 0<= y && y < N);
@@ -17,8 +20,13 @@ void initialize() {
   for (x=0; r < N; x++) {
     for (y=0; c < N; y++) {
       costShortestPath[x][y] = 999999; /* represents infinity */
+      visitedLocation[x][y] = false;
     }
   }
+}
+
+double heuristicDistance(int x, int y, int xG, int yG) {
+	return sqrt((pow(xG-x,2) + pow(yG-y,2))/5);
 }
 
 knightAStar(int x0, y0, xG, yG){
